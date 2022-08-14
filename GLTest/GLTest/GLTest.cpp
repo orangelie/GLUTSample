1f1d4c3915c423e4e33e7866fdbaf9259204756f
orangelie <sw031489@naver.com>
2022-08-15 03:39:14 +0900

    PROJECT FIN.
    PROJECT FIN.

---
+++ GLTest/GLTest/GLTest.cpp
@@ -0,0 +1,19 @@
+﻿#include "GLTest.h"
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(640, 320);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("glut Sample Project");
+
+    glutReshapeFunc(reshape);
+    glutDisplayFunc(display);
+    glutIdleFunc(update);
+
+    glEnable(GL_DEPTH_TEST);
+    glShadeModel(GL_SMOOTH);
+
+    glutMainLoop();
+}
---
+++ GLTest/GLTest/GLTest.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#pragma comment(lib, "glut32")
+
+#include <Windows.h>
+#include <windowsx.h>
+
+#include <glut.h>
+
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.5f, 0.0f);
+    glVertex3f(1.0f, -0.5f, 0.0f);
+    glVertex3f(-1.0f, -0.5f, 0.0f);
+    glEnd();
+
+    glFlush();
+    glutSwapBuffers();
+}
+
+void update()
+{
+    glutPostRedisplay();
+}
+
+void reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
